5a467722325a5ea96b7917e6153a79cf9292ea50
amhad3653-pixel <amhad3653@gmail.com>
2026-07-24 09:52:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Бот работает 🤖")
+    await update.message.reply_text("мава привет 👋")
 
 TOKEN = "8858025315:AAEGB7gbTP-wsNUcoUUiAoLZ92Rhp0bmRI8"
 
